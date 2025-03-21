fc9a0cf47408942672655642b25004721c85a52d
Samy FEREDJ <samyferedj@outlook.fr>
2025-03-21 18:58:58 +0100

    review controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :recette
   belongs_to :user
 
+  AUTHORIZED_RATINGS = (1..5)
+
   validates :commentaire, presence: true, length: { minimum: 10 }
-  validates :note, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :note, presence: true, inclusion: { in: AUTHORIZED_RATINGS }, numericality: { only_integer: true }
 end
