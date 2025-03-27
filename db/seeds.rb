d07ba466be6d720e9f748481eed643b10d462a5e
Samy FEREDJ <samyferedj@outlook.fr>
2025-03-27 20:19:27 +0100

    adding profile picture to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,21 +22,25 @@ img_recette2 = "https://jow.fr/_next/image?url=https%3A%2F%2Fstatic.jow.fr%2F304
 # ===== Users generation
 print "> User 1..."
 u1 = User.new(prenom: "Samy", nom: "Feredj", email: "samyferedj@outlook.fr", pseudo: "samydrate", poids: 90.2, taille: 194, age: 24, sexe: "Homme", objectif_physique: "Prise de muscle", password: "password")
+u1.photo.attach(io: URI.open("https://res.cloudinary.com/dgxnurfcj/image/upload/v1743102657/samy_of85uf.jpg"), filename: 'samy.jpeg', content_type: 'image/png')
 u1.save!
 puts "OK"
 #
 print "> User 2..."
 u2 = User.new(prenom: "Marcus", nom: "Meyer", email: "marcusmeyer@yahoo.fr", pseudo: "marcouss", poids: 80, taille: 191, age: 18, sexe: "Homme", objectif_physique: "Maintien", password: "password")
+u2.photo.attach(io: URI.open("https://res.cloudinary.com/dgxnurfcj/image/upload/v1743102638/marcus_lybqcl.png"), filename: 'marcus.png', content_type: 'image/png')
 u2.save!
 puts "OK"
 #
 print "> User 3..."
 u3 = User.new(prenom: "Arli", nom: "Sefgjini", email: "arlisefgjini@outlook.com", pseudo: "arlicoptere", poids: 60, taille: 130, age: 39, sexe: "Homme", objectif_physique: "Prise de muscle", password: "password")
+u3.photo.attach(io: URI.open("https://res.cloudinary.com/dgxnurfcj/image/upload/v1743102640/arli_upwlxd.png"), filename: 'arli.png', content_type: 'image/png')
 u3.save!
 puts "OK"
 #
 print "> User 4..."
 u4 = User.new(prenom: "Laurent", nom: "Millet", email: "laurentmillet@hotmail.fr", pseudo: "loron", poids: 159, taille: 169, age: 42, sexe: "Homme", objectif_physique: "Perte de poids", password: "password")
+u4.photo.attach(io: URI.open("https://res.cloudinary.com/dgxnurfcj/image/upload/v1743102639/laurent_jx3xmf.png"), filename: 'laurent.png', content_type: 'image/png')
 u4.save!
 puts "OK"
 
