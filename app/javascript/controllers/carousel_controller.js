1ac90d30f3422f2db2b11c0eedff77243fa1583d
Samy FEREDJ <samyferedj@outlook.fr>
2025-03-29 09:02:55 +0100

    merge master

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
-import { Swiper } from "swiper";
+import { Carousel } from "bootstrap"; // Import Bootstrap's Carousel class
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
   connect() {
+    console.log("coucou");
+
+    this.carousel = new Carousel(this.element.querySelector('.carousel'), {
+      touch: true, // Enable touch swiping
+      ride: 'carousel' // Optional, auto-start the carousel
+    });
   }
 
 
