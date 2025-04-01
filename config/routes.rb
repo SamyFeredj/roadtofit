32a60381a9f4326b455a12785c7bef70281404f8
Samy FEREDJ <samyferedj@outlook.fr>
2025-04-01 20:11:50 +0200

    retirer la page profile que j'ai essayé de créer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "dashboard", to: "pages#dashboard", as: :dashboard
-  get "profile", to: "pages#profile", as: :profile
 
   resources :posts do
     resources :likes, only: [:update]
