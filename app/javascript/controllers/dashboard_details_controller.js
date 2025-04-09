b2942ec7376cd7e86bc8319918ada6d6cee15548
Samy FEREDJ <samyferedj@outlook.fr>
2025-04-09 22:58:24 +0200

    infos user on dashboard

--- app/javascript/controllers/dashboard_details_controller.js
+++ app/javascript/controllers/dashboard_details_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dashboard-details"
 export default class extends Controller {
-  static targets = ["detailsIcon", "macros"]
+  static targets = ["detailsIcon", "macros", "infosIcon", "infos"]
 
   connect() {
   }
@@ -25,6 +25,25 @@ export default class extends Controller {
       this.detailsIconTarget.style = "transform: rotate(0);";
       this.detailsIconTarget.classList.add("toggle-icon-tobottom");
     }
+  }
 
+  toggleInfos(event) {
+    if (this.infosIconTarget.classList.contains("toggle-icon-tobottom")) {
+      // display
+      this.infosTarget.classList.toggle("display-details");
+      // animation svg
+      this.infosIconTarget.classList.add("playRotateLeft");
+      this.infosIconTarget.style = "transform: rotate(180deg);";
+      this.infosIconTarget.classList.remove("toggle-icon-tobottom");
+      this.infosIconTarget.classList.remove("playRotateRight");
+    } else {
+      // display
+      this.infosTarget.classList.toggle("display-details");
+      // animation svg
+      this.infosIconTarget.classList.add("playRotateRight");
+      this.infosIconTarget.classList.remove("playRotateLeft");
+      this.infosIconTarget.style = "transform: rotate(0);";
+      this.infosIconTarget.classList.add("toggle-icon-tobottom");
+    }
   }
 }
